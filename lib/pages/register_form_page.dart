3e2f388c5221150e045560cbb6457cad801f10ba
Oleksii <oleksii.sydorenko.dev@gmail.com>
2022-07-10 14:46:07 +0200

    StyleFieldForm

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -20,37 +20,96 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
           padding: EdgeInsets.all(16.0),
           children: [
             TextField(
-              decoration: InputDecoration(labelText: 'Full Name *'),
+              decoration: InputDecoration(
+                labelText: 'Full Name *',
+                hintText: 'What do people call you?',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                ),
+              ),
             ),
             SizedBox(
               height: 10,
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: "Phone Number *"),
+              decoration: InputDecoration(
+                labelText: "Phone Number *",
+                hintText: 'Where can we reach you? ',
+                helperText: 'Phone format: (XXX)XXXX-XXX',
+                prefixIcon: Icon(Icons.call),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                ),
+              ),
             ),
             SizedBox(
               height: 10,
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: "Email Address "),
+              decoration: InputDecoration(
+                labelText: "Email Address ",
+                hintText: 'Enter a email address',
+                icon: Icon(Icons.email),
+              ),
             ),
             SizedBox(
               height: 20,
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: "Life Story "),
+              decoration: InputDecoration(
+                labelText: "Life Story ",
+                hintText: 'Telll us about your self',
+                helperText: 'Keep it short, this is just a demo',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
             ),
             SizedBox(
               height: 20,
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: "Password *"),
+              decoration: InputDecoration(
+                labelText: "Password *",
+                hintText: 'Enter the password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+                icon: Icon(Icons.security),
+              ),
             ),
             SizedBox(
               height: 20,
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: "Confirm Password * "),
+              decoration: InputDecoration(
+                labelText: "Confirm Password * ",
+                hintText: 'Confirm the password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+                icon: Icon(Icons.border_color),
+              ),
             ),
             SizedBox(height: 15),
             RaisedButton(
