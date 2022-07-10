84d66e5d82a1a7c05bfdb9a200f970406e97c32e
Oleksii <oleksii.sydorenko.dev@gmail.com>
2022-07-10 13:48:23 +0200

    base_start

---
+++ lib/second_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  TextEditingController textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: TextField(
+              controller: textFieldController,
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          RaisedButton(
+            child: Text(
+              'Send text back',
+              style: TextStyle(fontSize: 24),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
